5fb864de01625a777f90347aecbff9ae6a45266c
antonindevidal <devidalantonin@gmail.com>
2023-10-13 19:10:22 +0200

    Created lib project

---
+++ Saint-Nec-Engine/Lib-Saint-Nec-Engine/Lib-Saint-Nec-Engine.cpp
@@ -0,0 +1,10 @@
+// Lib-Saint-Nec-Engine.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnLibSaintNecEngine()
+{
+}
