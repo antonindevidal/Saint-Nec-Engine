d0319474e6a75b79dad93406bdb16055340347c9
antonindevidal <devidalantonin@gmail.com>
2023-10-17 17:21:20 +0200

    Moved from Visual Studio to CMake

---
+++ Saint-Nec-Engine/Saint-Nec-Engine/src/Saint-Nec-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Saint-Nec-Engine.cpp : définit le point d'entrée de l'application.
+//
+#include <src/architecture/Component.h>
+#include "Saint-Nec-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
