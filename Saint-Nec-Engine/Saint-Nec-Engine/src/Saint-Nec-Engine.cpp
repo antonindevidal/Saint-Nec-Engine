256d666a791d5332e8ed95b7b90df2a99c4fd6fc
antonindevidal <devidalantonin@gmail.com>
2023-10-22 12:07:25 +0200

    Added imgui to cmake

--- Saint-Nec-Engine/Saint-Nec-Engine/src/Saint-Nec-Engine.cpp
+++ Saint-Nec-Engine/Saint-Nec-Engine/src/Saint-Nec-Engine.cpp
@@ -1,10 +1,9 @@
-﻿// Saint-Nec-Engine.cpp : définit le point d'entrée de l'application.
-//
-#include <architecture/Component.h>
-#include "Saint-Nec-Engine.h"
-#include <glad/glad.h>
+﻿#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
-
+#include <iostream>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -12,7 +11,38 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-int main()
+
+void draw_imgui(bool& show_demo_window, bool& show_another_window, ImVec4& clear_color, ImGuiIO& io)
+{
+    //Demo window (can be commented if needed)
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+    }
+
+}
+
+int main(void)
 {
     GLFWwindow* window;
 
@@ -20,6 +50,7 @@ int main()
     if (!glfwInit())
         return -1;
 
+    const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -42,6 +73,21 @@ int main()
     glViewport(0, 0, 640, 480);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    //Init ImGUI
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -49,12 +95,32 @@ int main()
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        draw_imgui(show_demo_window, show_another_window, clear_color, io);
+
+        //Render ImGUI
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
+
+
+
     }
+
+    // Cleanup ImGUI
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
