8e89e23a35c5089bb87c959370ed0ef00527b024
SomeOne768 <jalil0169@gmail.com>
2023-10-22 21:52:56 +0200

    :package: TEST init

---
+++ Saint-Nec-Engine/TEST/README.md
@@ -0,0 +1,3 @@
+# TP2
+This TP contains 2 different branches "main" and "alternative"
+which use a template conteneur and a conteneur type to calcul the barycentre
---
+++ Saint-Nec-Engine/TEST/test/test1.cpp
@@ -0,0 +1,23 @@
+// Entetes //---------------------------------------------------------------------------------------
+#include "catch.hpp"
+#include <cmath>
+// #include "../src/architecture/Component.h"
+#include "../../Saint-Nec-Engine-Lib/src/architecture/Component.h"
+#include <Physics/BoxCollider.hpp>
+
+// Tests //-----------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------ 1
+// TEST_CASE("TP2_Nuage::Ajout")
+// {
+//    Nuage<Cartesien> n;
+
+//    REQUIRE(n.size() == 0u);
+
+//    n.ajouter(Cartesien(12, 34));
+//    n.ajouter(Cartesien(56, 78));
+//    n.ajouter(Cartesien(90, 12));
+//    n.ajouter(Cartesien(34, 56));
+
+//    REQUIRE(n.size() == 4u);
+// }
