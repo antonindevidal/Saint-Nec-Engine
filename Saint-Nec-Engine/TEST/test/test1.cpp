a64b522dd9e75887ad46809019de2b378726b4ed
SomeOne768 <jalil0169@gmail.com>
2023-10-23 22:52:49 +0200

    :cross: problem import to check

--- Saint-Nec-Engine/TEST/test/test1.cpp
+++ Saint-Nec-Engine/TEST/test/test1.cpp
@@ -1,23 +1,12 @@
 // Entetes //---------------------------------------------------------------------------------------
-#include "catch.hpp"
+#include "../Dependencies/CATCH/catch.hpp"
 #include <cmath>
-// #include "../src/architecture/Component.h"
-#include "../../Saint-Nec-Engine-Lib/src/architecture/Component.h"
-#include <Physics/BoxCollider.hpp>
+#include <src/architecture/Component.h>
 
 // Tests //-----------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------ 1
-// TEST_CASE("TP2_Nuage::Ajout")
-// {
-//    Nuage<Cartesien> n;
-
-//    REQUIRE(n.size() == 0u);
-
-//    n.ajouter(Cartesien(12, 34));
-//    n.ajouter(Cartesien(56, 78));
-//    n.ajouter(Cartesien(90, 12));
-//    n.ajouter(Cartesien(34, 56));
-
-//    REQUIRE(n.size() == 4u);
-// }
+TEST_CASE("TEST FONCTIONNEMENT")
+{
+//    REQUIRE(true == !false);
+}
