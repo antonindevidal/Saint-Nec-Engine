d4dbb70aa7299c8e626722df0b4e4a89df2ca500
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-10-26 09:25:30 +0200

    correcting indentation

--- Saint-Nec-Engine/Saint-Nec-Engine-Lib/src/architecture/Vector3.hpp
+++ Saint-Nec-Engine/Saint-Nec-Engine-Lib/src/architecture/Vector3.hpp
@@ -1,23 +1,22 @@
 #pragma once
 
-
 namespace saintNecEngine
 {
-    class Vector3
+	class Vector3
 	{
 		double vect[3];
 
 	public:
 		Vector3() : Vector3(0, 0, 0) {}
-        
-        Vector3(const Vector3 &c)
-        {
-            vect[0] = c[0];
-            vect[1] = c[1];
-            vect[2] = c[2];
-        }
-		
-        Vector3(double x, double y, double z)
+
+		Vector3(const Vector3 &c)
+		{
+			vect[0] = c[0];
+			vect[1] = c[1];
+			vect[2] = c[2];
+		}
+
+		Vector3(double x, double y, double z)
 		{
 			vect[0] = x;
 			vect[1] = y;
