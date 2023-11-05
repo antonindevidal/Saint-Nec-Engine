d86087e490f4d90ef36e9254533069fa17e956ba
SomeOne768 <jalil0169@gmail.com>
2023-11-05 14:52:17 +0100

    ✅ matrice calcul problem solved

--- Saint-Nec-Engine/Saint-Nec-Engine-Test/test/testBox.cpp
+++ Saint-Nec-Engine/Saint-Nec-Engine-Test/test/testBox.cpp
@@ -118,84 +118,84 @@ TEST_CASE("TEST ROTATION 1 - center origin")
 
 }
 
-// TEST_CASE("TEST ROTATION 2 - center any")
-// {
+TEST_CASE("TEST ROTATION 2 - center any")
+{
 
-//     BoxCollider cube{
-//         glm::vec3{1, 1, 1}, 2, 2, 2};
+    BoxCollider cube{
+        glm::vec3{1, 1, 1}, 2, 2, 2};
 
-//     std::vector<glm::vec3> cube_points_before_rotation = {
-//         glm::vec3{0, 0, 0},
-//         glm::vec3{0, 0, 2},
-//         glm::vec3{0, 2, 0},
-//         glm::vec3{0, 2, 2},
-//         glm::vec3{2, 0, 0},
-//         glm::vec3{2, 0, 2},
-//         glm::vec3{2, 2, 0},
-//         glm::vec3{2, 2, 2}};
+    std::vector<glm::vec3> cube_points_before_rotation = {
+        glm::vec3{0, 0, 0},
+        glm::vec3{0, 0, 2},
+        glm::vec3{0, 2, 0},
+        glm::vec3{0, 2, 2},
+        glm::vec3{2, 0, 0},
+        glm::vec3{2, 0, 2},
+        glm::vec3{2, 2, 0},
+        glm::vec3{2, 2, 2}};
 
-//     CHECK(cube.hasPoint(cube_points_before_rotation[0]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[1]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[2]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[3]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[4]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[5]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[6]) == true);
-//     CHECK(cube.hasPoint(cube_points_before_rotation[7]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[0]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[1]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[2]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[3]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[4]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[5]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[6]) == true);
+    CHECK(cube.hasPoint(cube_points_before_rotation[7]) == true);
 
-//     cube.setRotation({M_PI, 0, 0});
-//     std::vector<glm::vec3> cube_points_after_rotation = cube_points_before_rotation;
+    cube.setRotation({M_PI, 0, 0});
+    std::vector<glm::vec3> cube_points_after_rotation = cube_points_before_rotation;
 
-//     CHECK(cube.contains(cube_points_after_rotation[0]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[1]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[2]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[3]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[4]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[5]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[6]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[7]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[0]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[1]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[2]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[3]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[4]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[5]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[6]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[7]) == true);
 
-//     cube.setRotation({0, M_PI, 0});
+    cube.setRotation({0, M_PI, 0});
 
-//     CHECK(cube.contains(cube_points_after_rotation[0]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[1]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[2]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[3]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[4]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[5]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[6]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[7]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[0]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[1]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[2]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[3]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[4]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[5]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[6]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[7]) == true);
 
-//     cube.setRotation({0, 0, M_PI});
+    cube.setRotation({0, 0, M_PI});
 
-//     CHECK(cube.contains(cube_points_after_rotation[0]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[1]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[2]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[3]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[4]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[5]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[6]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[7]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[0]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[1]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[2]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[3]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[4]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[5]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[6]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[7]) == true);
 
-//     cube.setRotation({M_PI, M_PI, M_PI});
-//     CHECK(cube.contains(cube_points_after_rotation[0]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[1]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[2]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[3]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[4]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[5]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[6]) == true);
-//     CHECK(cube.contains(cube_points_after_rotation[7]) == true);
+    cube.setRotation({M_PI, M_PI, M_PI});
+    CHECK(cube.contains(cube_points_after_rotation[0]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[1]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[2]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[3]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[4]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[5]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[6]) == true);
+    CHECK(cube.contains(cube_points_after_rotation[7]) == true);
 
-//     BoxCollider b2{
-//         glm::vec3{2.1, 0, 0}, 2, 2, 2};
-//     std::vector<glm::vec3> expected_points = {
-//         glm::vec3{0, 0, 0},
-//         glm::vec3{0, 0, 2},
-//         glm::vec3{0, 2, 0},
-//         glm::vec3{0, 2, 2},
-//         glm::vec3{2, 0, 0},
-//         glm::vec3{2, 0, 2},
-//         glm::vec3{2, 2, 0},
-//         glm::vec3{2, 2, 2}};
-// }
+    BoxCollider b2{
+        glm::vec3{2.1, 0, 0}, 2, 2, 2};
+    std::vector<glm::vec3> expected_points = {
+        glm::vec3{0, 0, 0},
+        glm::vec3{0, 0, 2},
+        glm::vec3{0, 2, 0},
+        glm::vec3{0, 2, 2},
+        glm::vec3{2, 0, 0},
+        glm::vec3{2, 0, 2},
+        glm::vec3{2, 2, 0},
+        glm::vec3{2, 2, 2}};
+}
