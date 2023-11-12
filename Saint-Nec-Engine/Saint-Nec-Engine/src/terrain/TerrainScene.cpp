f79155338d434fe6972e191f259b2f0e6ffd9c84
antonindevidal <devidalantonin@gmail.com>
2023-11-12 11:18:21 +0100

    Modified texture add calls

--- Saint-Nec-Engine/Saint-Nec-Engine/src/terrain/TerrainScene.cpp
+++ Saint-Nec-Engine/Saint-Nec-Engine/src/terrain/TerrainScene.cpp
@@ -12,8 +12,8 @@ void TerrainScene::load()
 	sne::GameObject* terrain = new sne::GameObject();
 	terrain->setName("Terrain");
 	addGameObject(terrain);	
-	sne::graphics::Plane* plane = new sne::graphics::Plane(20, 20, 5, "resources/shaders/terrain/basic_terrain.vert", "resources/shaders/terrain/basic_terrain.frag");
-	plane->setTexture("resources/textures/heightmap_puy_de_dome.jpg","heightmap");
+	sne::graphics::Plane* plane = new sne::graphics::Plane(300, 300, 5, "resources/shaders/terrain/basic_terrain.vert", "resources/shaders/terrain/basic_terrain.frag");
+	plane->addTexture("resources/textures/noise.jpg","heightmap");
 	terrain->addComponent(plane);
 
 
@@ -23,6 +23,7 @@ void TerrainScene::load()
 	cubeMonoColor->setName("Monocolor cube");
 	cubeMonoColor->addComponent(new sne::graphics::Cube("resources/shaders/color.vert", "resources/shaders/color.frag", glm::vec3{ 0.5,0.2,0.6 }));
 	cubeMonoColor->translate(glm::vec3{ 0.0f, 0.0f, 0.0f });
+
 	
 }
 
