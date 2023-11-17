1ae97a1798ec002ba5b854634419874e7675cfbd
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-11-17 17:41:53 +0100

    init physic func

---
+++ Saint-Nec-Engine/Saint-Nec-Engine-Lib/src/physics/util.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @file util.hpp
+ * @brief physics usefull functions
+ * @author Abdeljalil ZOGHLAMI
+ * @date november 17th 2023.
+ */
+
+#include <glm/glm.hpp>
+
+
+namespace sne
+{
+
+    namespace saintNecPhysics
+    {
+        glm::vec3 getNormal(const glm::vec3&, const glm::vec3);
+    }   
+}
