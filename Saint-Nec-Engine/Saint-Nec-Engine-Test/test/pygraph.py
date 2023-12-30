1627667e455e223d4bbffee57570076cc32768ff
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-12-30 20:32:01 +0100

    3D visualisation

--- Saint-Nec-Engine/Saint-Nec-Engine-Test/test/pygraph.py
+++ Saint-Nec-Engine/Saint-Nec-Engine-Test/test/pygraph.py
@@ -1,10 +1,13 @@
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 PATH = "./data/"
 FILENAME = "testInteraction.txt"
 PRINT_LIKE_POINT = True
+_3DPRINT = True
+
 
 X = []
 Y = []
@@ -25,56 +28,68 @@ with open(PATH + FILENAME, "r", encoding="utf-8") as f:
         i+=1
 
 
-plt.subplot(3, 2, 1)
-plt.scatter(T, X, color='red', marker= 'o' if PRINT_LIKE_POINT else':')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title('Current X')
-plt.subplot(3, 2, 3)
-plt.scatter(T, Y, color='red', marker='o' if PRINT_LIKE_POINT else':')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title('Current Y')
-plt.subplot(3, 2, 5)
-plt.scatter(T, Z, color='red', marker='o' if PRINT_LIKE_POINT else':')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title('Current Z')
+if _3DPRINT:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(X, Y, Z, marker='o')  
+    
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('Trajectoire en 3D')
+
+else:
+
+    plt.subplot(3, 2, 1)
+    plt.scatter(T, X, color='red', marker= 'o' if PRINT_LIKE_POINT else':')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title('Current X')
+    plt.subplot(3, 2, 3)
+    plt.scatter(T, Y, color='red', marker='o' if PRINT_LIKE_POINT else':')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title('Current Y')
+    plt.subplot(3, 2, 5)
+    plt.scatter(T, Z, color='red', marker='o' if PRINT_LIKE_POINT else':')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title('Current Z')
 
 
-a = np.array([0.0, 0.0, -9.81])
-v = np.array([0.0, 0.0, 0.0])
-p = np.array([0.0, 0.0, 0.0])
+    a = np.array([0.0, 0.0, -9.81])
+    v = np.array([0.0, 0.0, 0.0])
+    p = np.array([0.0, 0.0, 0.0])
 
-# t = sqrt(2)
-# t = dt
+    # t = sqrt(2)
+    # t = dt
 
-print(dt)
+    print(dt)
 
-def compute(t):
-    global a, v, p
-    p += v * t + 0.5 * a * t * t
-    v += a * t
-    return p
+    def compute(t):
+        global a, v, p
+        p += v * t + 0.5 * a * t * t
+        v += a * t
+        return p
 
-Expected = [[0.0, 0.0, 0.0]] + [list(compute(dt)) for _ in range(len(T)-1)]
+    Expected = [[0.0, 0.0, 0.0]] + [list(compute(dt)) for _ in range(len(T)-1)]
 
-plt.subplot(3, 2, 2)
-plt.scatter(T, [x for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title("Expected X")
-plt.subplot(3, 2, 4)
-plt.scatter(T, [y for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title("Expected Y")
-plt.subplot(3, 2, 6)
-plt.scatter(T, [z for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
-plt.xlabel('Time (s)')
-plt.ylabel('Altitude (m)')
-plt.title("Expected Z")
+    plt.subplot(3, 2, 2)
+    plt.scatter(T, [x for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title("Expected X")
+    plt.subplot(3, 2, 4)
+    plt.scatter(T, [y for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title("Expected Y")
+    plt.subplot(3, 2, 6)
+    plt.scatter(T, [z for x,y,z in Expected], color='red', marker='o', label='Z variations other time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Altitude (m)')
+    plt.title("Expected Z")
 
-plt.subplots_adjust(hspace=1, wspace=0.5)
+    plt.subplots_adjust(hspace=1, wspace=0.5)
+
 plt.show()
-
