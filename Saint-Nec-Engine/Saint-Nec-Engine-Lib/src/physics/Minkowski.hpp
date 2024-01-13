7492ebdd37bc140067cf0d247de83bcecae3fdbd
SomeOne768 <81550632+SomeOne768@users.noreply.github.com>
2024-01-13 19:19:09 +0100

    adding Minkowski element

---
+++ Saint-Nec-Engine/Saint-Nec-Engine-Lib/src/physics/Minkowski.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <architecture/util.hpp>
+
+namespace sne::physics
+{
+	class Minkowski {
+		glm::vec3 _pointA,
+			_pointB,
+			_coordinate;
+		float _distance; 
+
+	public:
+		Minkowski(const glm::vec3& pointA, const glm::vec3& pointB)
+		{
+			_pointA = pointA;
+			_pointB = pointB;
+			_coordinate = _pointA - _pointB;
+			_distance = norm(_coordinate);
+		}
+
+		float getDistance() const
+		{
+			return _distance;
+		}
+
+	};
+
+}
