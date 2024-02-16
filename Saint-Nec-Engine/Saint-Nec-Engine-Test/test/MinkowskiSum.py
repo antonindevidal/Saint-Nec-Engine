38fad7acf27d47005a3611944872863b1c76d6cf
SomeOne768 <jalil0169@gmail.com>
2024-02-17 00:48:08 +0100

    adding 3D visualization

--- Saint-Nec-Engine/Saint-Nec-Engine-Test/test/MinkowskiSum.py
+++ Saint-Nec-Engine/Saint-Nec-Engine-Test/test/MinkowskiSum.py
@@ -1,88 +1,154 @@
+#!/usr/bin/python3
 from turtle import color
 import numpy as np 
 import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
 
+_2D = False
 
+# 2D parameters
 FIGURE_1 = [(0,-1), (0,1), (1.5,0)]
 FIGURE_2 = [(0.5,0), (2,-1), (2,1)]
-POINTS = set()
+
+# 3D parameters
+CENTER1 = (0, 0, 0)
+CENTER2 = (2.1,0,0)
+
+if _2D:    
+    POINTS = set()
+
+    def MinkowskiSum(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append( (point[0] + point2[0], point[1] + point2[1]) )
+        
+        return POINTS
+
+    def MinkowskiDifference(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append( (point[0] - point2[0], point[1] - point2[1]) )
+        
+        return POINTS
+
+    plt.subplot(2,2,1)
+    plt.plot([x for x,y in FIGURE_1] + [FIGURE_1[0][0]] , [y for x,y in FIGURE_1]+ [FIGURE_1[0][1]], marker='o')
+    plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o', color='red')
+    plt.title("Figures")
+    # plt.subplot(2,2 ,2)
+    # plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o')
+    plt.subplot(2,2,3)
+    POINTS = MinkowskiSum(FIGURE_1, FIGURE_2)
+    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
+    plt.title("MinkowskiSum")
+    plt.subplot(2,2,4)
+    POINTS = MinkowskiDifference(FIGURE_1, FIGURE_2)
+    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
+    plt.title("MinkowskiDifference")
+    plt.show()
+    # print(POINTS)
 
 
-def MinkowskiSum(Shape1, Shape2):
-    POINTS = []
-    for point in Shape1:
-        for point2 in Shape2:
-            POINTS.append( (point[0] + point2[0], point[1] + point2[1]) )
-    
-    return POINTS
-
-def MinkowskiDifference(Shape1, Shape2):
-    POINTS = []
-    for point in Shape1:
-        for point2 in Shape2:
-            POINTS.append( (point[0] - point2[0], point[1] - point2[1]) )
-    
-    return POINTS
-
-plt.subplot(2,2,1)
-plt.plot([x for x,y in FIGURE_1] + [FIGURE_1[0][0]] , [y for x,y in FIGURE_1]+ [FIGURE_1[0][1]], marker='o')
-plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o', color='red')
-plt.title("Figures")
-# plt.subplot(2,2 ,2)
-# plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o')
-plt.subplot(2,2,3)
-POINTS = MinkowskiSum(FIGURE_1, FIGURE_2)
-plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-plt.title("MinkowskiSum")
-plt.subplot(2,2,4)
-POINTS = MinkowskiDifference(FIGURE_1, FIGURE_2)
-plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-plt.title("MinkowskiDifference")
-plt.show()
-# print(POINTS)
 
 
+    def Cube2D(center, size):
+        delta = size / 2
+        return [(center[0]-delta, center[1]-delta),
+                (center[0]-delta, center[1]+delta),
+                (center[0]+delta, center[1]-delta),
+                (center[0]+delta, center[1]+delta)]
 
+    CUBE_1 = Cube2D((0, 0), 2)
+    CUBE_2 = Cube2D((1.3, 0.3), 2)
+    POINTS = set()
 
-def Cube2D(center, size):
-    delta = size / 2
-    return [(center[0]-delta, center[1]-delta),
-            (center[0]-delta, center[1]+delta),
-            (center[0]+delta, center[1]-delta),
-            (center[0]+delta, center[1]+delta)]
+    def MinkowskiSum(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append( (point[0] + point2[0], point[1] + point2[1]) )
+        
+        return POINTS
 
-CUBE_1 = Cube2D((0, 0), 2)
-CUBE_2 = Cube2D((1.3, 0.3), 2)
-POINTS = set()
+    def MinkowskiDifference(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append( (point[0] - point2[0], point[1] - point2[1]) )
+        
+        return POINTS
 
-def MinkowskiSum(Shape1, Shape2):
-    POINTS = []
-    for point in Shape1:
-        for point2 in Shape2:
-            POINTS.append( (point[0] + point2[0], point[1] + point2[1]) )
-    
-    return POINTS
+    plt.subplot(2,2,1)
+    plt.plot([x for x,y in CUBE_1] + [CUBE_1[0][0]] , [y for x,y in CUBE_1]+ [CUBE_1[0][1]], marker='o')
+    plt.plot([x for x,y in CUBE_2] + [CUBE_2[0][0]], [y for x,y in CUBE_2]+ [CUBE_2[0][1]], marker='o', color='red')
+    plt.title("Figures")
+    # plt.subplot(2,2 ,2)
+    # plt.plot([x for x,y in CUBE_2] + [CUBE_2[0][0]], [y for x,y in CUBE_2]+ [CUBE_2[0][1]], marker='o')
+    plt.subplot(2,2,3)
+    POINTS = MinkowskiSum(CUBE_1, CUBE_2)
+    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
+    plt.title("MinkowskiSum")
+    plt.subplot(2,2,4)
+    POINTS = MinkowskiDifference(CUBE_1, CUBE_2)
+    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
+    plt.title("MinkowskiDifference")
+    plt.show()
+else:
 
-def MinkowskiDifference(Shape1, Shape2):
-    POINTS = []
-    for point in Shape1:
-        for point2 in Shape2:
-            POINTS.append( (point[0] - point2[0], point[1] - point2[1]) )
-    
-    return POINTS
+    def MinkowskiSum(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append((point[0] + point2[0], point[1] + point2[1], point[2] + point2[2]))
 
-plt.subplot(2,2,1)
-plt.plot([x for x,y in CUBE_1] + [CUBE_1[0][0]] , [y for x,y in CUBE_1]+ [CUBE_1[0][1]], marker='o')
-plt.plot([x for x,y in CUBE_2] + [CUBE_2[0][0]], [y for x,y in CUBE_2]+ [CUBE_2[0][1]], marker='o', color='red')
-plt.title("Figures")
-# plt.subplot(2,2 ,2)
-# plt.plot([x for x,y in CUBE_2] + [CUBE_2[0][0]], [y for x,y in CUBE_2]+ [CUBE_2[0][1]], marker='o')
-plt.subplot(2,2,3)
-POINTS = MinkowskiSum(CUBE_1, CUBE_2)
-plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-plt.title("MinkowskiSum")
-plt.subplot(2,2,4)
-POINTS = MinkowskiDifference(CUBE_1, CUBE_2)
-plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-plt.title("MinkowskiDifference")
-plt.show()
+        return POINTS
+
+    def MinkowskiDifference(Shape1, Shape2):
+        POINTS = []
+        for point in Shape1:
+            for point2 in Shape2:
+                POINTS.append((point[0] - point2[0], point[1] - point2[1], point[2] - point2[2]))
+
+        return POINTS
+
+    def plot_shape(ax, shape, color):
+        x, y, z = zip(*shape)
+        ax.scatter(x, y, z, c=color, marker='o')
+
+    def Cube3D(center, size):
+        delta = size / 2
+        return [
+            (center[0] - delta, center[1] - delta, center[2] - delta),
+            (center[0] - delta, center[1] - delta, center[2] + delta),
+            (center[0] - delta, center[1] + delta, center[2] - delta),
+            (center[0] - delta, center[1] + delta, center[2] + delta),
+            (center[0] + delta, center[1] - delta, center[2] - delta),
+            (center[0] + delta, center[1] - delta, center[2] + delta),
+            (center[0] + delta, center[1] + delta, center[2] - delta),
+            (center[0] + delta, center[1] + delta, center[2] + delta)
+        ]
+
+    CUBE_1 = Cube3D(CENTER1, 2)
+    CUBE_2 = Cube3D(CENTER2, 2)
+
+    POINTS_SUM = MinkowskiSum(CUBE_1, CUBE_2)
+    POINTS_DIFF = MinkowskiDifference(CUBE_1, CUBE_2)
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(2, 2, 1, projection='3d')
+    plot_shape(ax1, CUBE_1, 'b')
+    plot_shape(ax1, CUBE_2, 'r')
+    ax1.set_title("Figures")
+
+    ax2 = fig.add_subplot(2, 2, 3, projection='3d')
+    plot_shape(ax2, POINTS_SUM, 'g')
+    ax2.set_title("Minkowski Sum")
+
+    ax3 = fig.add_subplot(2, 2, 4, projection='3d')
+    plot_shape(ax3, POINTS_DIFF, 'y')
+    ax3.set_title("Minkowski Difference")
+
+    plt.show()
