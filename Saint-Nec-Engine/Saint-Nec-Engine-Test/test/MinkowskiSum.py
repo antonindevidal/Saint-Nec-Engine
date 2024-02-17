34a6d38c7caea71aaff0c8aac264460302a3d771
SomeOne768 <jalil0169@gmail.com>
2024-02-17 02:22:34 +0100

    clean

--- Saint-Nec-Engine/Saint-Nec-Engine-Test/test/MinkowskiSum.py
+++ Saint-Nec-Engine/Saint-Nec-Engine-Test/test/MinkowskiSum.py
@@ -4,15 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 
-_2D = False
+_2D = True
+TAILLE = 2
 
 # 2D parameters
-FIGURE_1 = [(0,-1), (0,1), (1.5,0)]
-FIGURE_2 = [(0.5,0), (2,-1), (2,1)]
+FIGURE_1 = (0,0)
+FIGURE_2 = (4,0)
 
 # 3D parameters
 CENTER1 = (0, 0, 0)
-CENTER2 = (2.1,0,0)
+CENTER2 = (4, 0, 0)
 
 if _2D:    
     POINTS = set()
@@ -33,26 +34,6 @@ if _2D:
         
         return POINTS
 
-    plt.subplot(2,2,1)
-    plt.plot([x for x,y in FIGURE_1] + [FIGURE_1[0][0]] , [y for x,y in FIGURE_1]+ [FIGURE_1[0][1]], marker='o')
-    plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o', color='red')
-    plt.title("Figures")
-    # plt.subplot(2,2 ,2)
-    # plt.plot([x for x,y in FIGURE_2] + [FIGURE_2[0][0]], [y for x,y in FIGURE_2]+ [FIGURE_2[0][1]], marker='o')
-    plt.subplot(2,2,3)
-    POINTS = MinkowskiSum(FIGURE_1, FIGURE_2)
-    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-    plt.title("MinkowskiSum")
-    plt.subplot(2,2,4)
-    POINTS = MinkowskiDifference(FIGURE_1, FIGURE_2)
-    plt.plot([x for x,y in POINTS], [y for x,y in POINTS], marker='o')
-    plt.title("MinkowskiDifference")
-    plt.show()
-    # print(POINTS)
-
-
-
-
     def Cube2D(center, size):
         delta = size / 2
         return [(center[0]-delta, center[1]-delta),
@@ -60,8 +41,8 @@ if _2D:
                 (center[0]+delta, center[1]-delta),
                 (center[0]+delta, center[1]+delta)]
 
-    CUBE_1 = Cube2D((0, 0), 2)
-    CUBE_2 = Cube2D((1.3, 0.3), 2)
+    CUBE_1 = Cube2D(FIGURE_1, TAILLE)
+    CUBE_2 = Cube2D(FIGURE_2, TAILLE)
     POINTS = set()
 
     def MinkowskiSum(Shape1, Shape2):
@@ -130,8 +111,8 @@ else:
             (center[0] + delta, center[1] + delta, center[2] + delta)
         ]
 
-    CUBE_1 = Cube3D(CENTER1, 2)
-    CUBE_2 = Cube3D(CENTER2, 2)
+    CUBE_1 = Cube3D(CENTER1, TAILLE)
+    CUBE_2 = Cube3D(CENTER2, TAILLE)
 
     POINTS_SUM = MinkowskiSum(CUBE_1, CUBE_2)
     POINTS_DIFF = MinkowskiDifference(CUBE_1, CUBE_2)
